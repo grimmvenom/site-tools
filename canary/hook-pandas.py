1a1a91a7e2edf66503890e9c8451f138b551d97d
Grimm Venom <grimmvenom@gmail.com>
2018-09-05 22:08:45 -0400

    Update hook-pandas.py
    Included default file locations so it easier to compile in the future

--- canary/hook-pandas.py
+++ canary/hook-pandas.py
@@ -11,6 +11,16 @@
 """
 Fixes issue #2978 with pandas version 0.21
 (Pandas missing `pandas._libs.tslibs.timedeltas.so`)
+
+MacOSX:
+/usr/local/lib/python3.6/site-packages/PyInstaller/hooks/hook-pandas.py
+
+Windows:
+%AppData%\Local\Programs\Python\Python36\Lib\site-packages\Pyinstaller\hooks\hook-pandas.py
+
+Linux:
+/usr/lib/python3.6/site-packages/PyInstaller/hooks/hook-pandas.py
+/usr/local/lib/python3.6/site-packages/PyInstaller/hooks/hook-pandas.py
 """
 
 
